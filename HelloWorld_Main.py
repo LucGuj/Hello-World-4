8b3d5dcae8bf9064ee4f2e2186a467b5c02ccc06
Guj <U549496@DOW.COM>
2020-09-02 15:21:28 +0200

    VERSION2

--- HelloWorld_Main.py
+++ HelloWorld_Main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0,3.14,500)
-y = np.sin(x)*np.cos(x**2.)*np.cos(x)**2.0
+y = np.sin(x)*np.cos(x**2.)*np.cos(x)**2.0 + 150.25
 
 plt.plot(x,y)
 plt.grid()
